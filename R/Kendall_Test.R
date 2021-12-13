6d363806a31e5020cfbdb9ab91313f818d68a87a
aandishah <aandishah@gmail.com>
2021-12-13 18:21:07 -0500

    Change structure

---
+++ R/Kendall_Test.R
@@ -0,0 +1,53 @@
+library(ncdf4)
+library(spatialEco)
+
+#Setting up the path to data
+
+ppath <- "C:/Users/aandi/Desktop/Fall_2021/GEOG_399/Datasets/" # point to dataset folder, USE NOT HARD PATH
+ncpath <- ppath
+
+#Low Cloud Area Fraction
+ncname1 <- "lcdc.mon.mean"
+ncfname1 <- paste(ncpath, ncname1, ".nc", sep="")
+dname1 <- "lcdc"
+
+# open a netCDF file
+ncin <- nc_open(ncfname1)
+lon <- ncvar_get(ncin, "x")
+lat <- ncvar_get(ncin, "y")
+time <- ncvar_get(ncin,"time")
+tunits <- ncatt_get(ncin,"time","units")
+nt <- dim(time)
+
+##############################################################################
+##############################################################################
+#raster brick to data frame
+var.nc <- brick(ncfname1, varname=dname1, layer="time") #reopen netcdf file as Raster brick for TIME variable
+var.nc
+
+#Extracting first Year of Low Cloud Data
+
+lcdc_1979 <- var.nc[[1:12]]
+
+lcdc_kendall_test <- raster.kendall(lcdc_1979,
+                                    tau = TRUE,
+                                    p.value = TRUE,
+                                    z.value = TRUE,
+                                    confidence = TRUE,
+                                    intercept = TRUE,
+                                    prewhiten = FALSE,
+                                    )
+
+plot(lcdc_kendall_test)
+plot(lcdc_kendall_test$p.value)
+
+p_Value <- lcdc_kendall_test[[3]]
+High_sig <- p_Value > 0.05
+
+
+cols <- c("tan", "yellow3", "green4")
+plot_noaxes(High_sig, legend = FALSE, main = "High Signifance", col = cols,
+            mar = c(0, 0, 1, 0))
+
+
+
---
+++ external/notebook/Opening_NARR.R
@@ -0,0 +1,74 @@
+library(ncdf4)
+library(chron)
+#library(raster)
+library(lattice)
+library(RColorBrewer)
+
+# set path and filename
+
+ppath <- "C:/Users/aandi/Desktop/Fall_2021/GEOG_399/Datasets/" # point to dataset folder, USE NOT HARD PATH
+ncpath <- ppath
+
+#High Cloud Area Fraction
+ncname1 <- "hcdc.mon.mean"
+ncfname1 <- paste(ncpath, ncname1, ".nc", sep="")
+dname1 <- "hcdc"
+
+# open a netCDF file
+ncin <- nc_open(ncfname1)
+print(ncin)
+
+# get x's and y's
+x <- ncvar_get(ncin,"x")
+xlname <- ncatt_get(ncin, "x", "long_name")
+nx <- dim(x)
+xunits <- ncatt_get(ncin, "x", "units")
+head(x)
+
+y <- ncvar_get(ncin,"y")
+ylname <- ncatt_get(ncin, "y", "long_name")
+yunits <- ncatt_get(ncin, "y", "units")
+ny <- dim(y)
+head(y)
+
+print(c(nx, ny))
+
+# get time
+time <- ncvar_get(ncin, "time")
+time
+
+tunits <- ncatt_get(ncin, "time", "units")
+nt <- dim(time)
+nt
+
+
+# get high cloud fraction
+hcdc_array <- ncvar_get(ncin, dname1)
+dlname <- ncatt_get(ncin, dname1, "long_name")
+dunits <- ncatt_get(ncin, dname1, "units")
+fillvalue <- ncatt_get(ncin, dname1, "_FillValue")
+dim(hcdc_array)
+
+#Slice of first 12 months
+hcdc_slice12 <- hcdc_array[,,1:12] #Changing the 1:2, changes the number of time bands we are using
+dim(hcdc_slice12)
+
+
+# replace netCDF fill values with NA's
+hcdc_array[hcdc_array==fillvalue$value] <- NA
+length(na.omit(as.vector(hcdc_array[,,1])))
+
+
+# get a single slice or layer (January)
+m <- 1
+hc_slice <- hcdc_array[,,m]
+
+# create dataframe -- reshape data
+# matrix (nlon*nlat rows by 2 cols) of lons and lats
+lonlat <- as.matrix(expand.grid(lon,lat))
+dim(lonlat)
+
+# vector of `HC` values
+hc_vec <- as.vector(hc_slice)
+length(hc_vec)
+
